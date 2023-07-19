c9f04c9b632b0e644cf004d1645bc841c5173a07
anhkien789 <33304061+anhkien789@users.noreply.github.com>
2023-07-19 15:51:36 +0700

    add login, logout, signup

--- leads/views.py
+++ leads/views.py
@@ -1,12 +1,19 @@
-from django.core.mail import send_mail 
+from django.core.mail import send_mail
 from django.forms.models import BaseModelForm
 from django.shortcuts import render, redirect, resolve_url
 from django.http import HttpResponse
 from django.views import generic
 from .models import Lead, Agent
-from .forms import LeadForm, LeadModelForm
+from .forms import LeadForm, LeadModelForm, CustomUserCreationForm 
 
 # Create your views here.
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self) -> str:
+        return resolve_url("login")
+
 class LandingPageView(generic.TemplateView):
     template_name = "landing.html"
 
