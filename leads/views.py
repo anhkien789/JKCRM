9871d8eb11c6410bf631b5181d4167e00f3b5224
anhkien789 <33304061+anhkien789@users.noreply.github.com>
2023-07-21 19:08:35 +0700

    complete feature that assign agent to lead and then create category for lead

--- leads/views.py
+++ leads/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.views import generic
 from agents.mixins import OrganisorAndLoginRequiredMixin
 from .models import Lead, Agent
-from .forms import LeadForm, LeadModelForm, CustomUserCreationForm 
+from .forms import LeadForm, LeadModelForm, CustomUserCreationForm, AssignAgentForm
 
 # Create your views here.
 class SignupView(generic.CreateView):
@@ -155,6 +155,27 @@ def lead_delete(request, pk):
     lead = Lead.objects.get(id=pk)
     lead.delete()
     return redirect('/leads')
+
+class AssignAgentView(OrganisorAndLoginRequiredMixin, generic.FormView):
+    template_name= "leads/assign_agent.html"
+    form_class = AssignAgentForm
+
+    def get_form_kwargs(self, **kwargs):
+        kwargs = super(AssignAgentView, self).get_form_kwargs(**kwargs)
+        kwargs.update({
+            "request": self.request
+        })
+        return kwargs
+
+    def get_success_url(self) -> str:
+        return resolve_url("leads:lead-list")
+    
+    def form_valid(self, form):
+        agent = form.cleaned_data['agent']
+        lead = Lead.objects.get(id=self.kwargs['pk'])
+        lead.agent = agent
+        lead.save()
+        return super(AssignAgentView, self).form_valid(form)
 
 # def lead_update(request, pk):
 #     lead = Lead.objects.get(id=pk)
