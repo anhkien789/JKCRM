7adfc6db04f4a7c254f357c3241c28db7bfe92cf
anhkien789 <33304061+anhkien789@users.noreply.github.com>
2023-07-23 20:11:33 +0700

    setting some configuration

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # JKCRM
+
 Portfolio for CRM appication
+
+# Settings
+
+To run the web application, you have to contact the owner's source code in order to get the env file. Then, you need follow the steps below:
+
+- open Terminal
+- run command: "export READ_DOT_ENV_FILE=True"
+- after that, we can run command: "python manage.py runserver"
--- jkcrm/settings.py
+++ jkcrm/settings.py
@@ -11,8 +11,10 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR / "jkcrm", '.env'))
+READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default=False)
+
+if READ_DOT_ENV_FILE:
+    environ.Env.read_env(os.path.join(BASE_DIR / "jkcrm", '.env'))
 
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
@@ -23,6 +25,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'jkcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
@@ -122,6 +130,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 STATIC_ROOT = "static_root"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
