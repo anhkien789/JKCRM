128ab7dab627b853c4f5e5421e48fa1765da3d37
anhkien789 <33304061+anhkien789@users.noreply.github.com>
2023-07-23 18:21:09 +0700

    setting environmental variables

--- jkcrm/settings.py
+++ jkcrm/settings.py
@@ -1,17 +1,21 @@
 from pathlib import Path
 
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR / "jkcrm", '.env'))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)p92an(fkxz)*eh!46l=y$(=n)*&nu600r=crzku%)e(#z7=!j'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = []
 
